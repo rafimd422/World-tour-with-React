6c15a1db7003cbe5c7905e1007c72abaa792f5c6
rafimd422 <rafimf2222@gmail.com>
2023-09-10 22:22:18 +0600

    Added new section dynamically

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,6 +3,7 @@ import Country from "./country";
 
 const Countries = () => {
 const [countries,setCountries] = useState([])
+const [visitedCountry,setvisitedCountry] = useState([])
 
 useEffect(()=>{
     fetch('https://restcountries.com/v3.1/all')
@@ -10,14 +11,21 @@ useEffect(()=>{
     .then(data=> setCountries(data))
 },[])
 
+const handlevisitedCountry = (country) =>{
+const pushVisited = [...visitedCountry, country];
+setvisitedCountry(pushVisited)
+}
   return (
     <>
      <h1>Countreis: {countries.length}</h1>
+     <div>
+     <h2>Countried I've visited...</h2>
+     <ul>{visitedCountry.map(country => <li>{country.name.common}</li>)}</ul>
+     </div>
          <div className="countryParent">
-      {countries.map(country => <Country key={country.cca3} country={country}></Country>)}
+      {countries.map(country => <Country key={country.cca3} country={country} handlevisitedCountry = {handlevisitedCountry}></Country>)}
     </div>
     </>
-
   )
 }
 export default Countries;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 
-const Country = ({country}) => {
+const Country = ({country,handlevisitedCountry}) => {
    const {name,flags,population,area,cca3} = country;
     console.log(country)
 
 
-    const [visited, setVisited] = useState(true)
+const [visited, setVisited] = useState(true)
 const handleClick = () =>{
   setVisited(!visited)
 }
 
+const visitedCountry = () => handlevisitedCountry(country)
 
   return (
     <div className={!visited? 'clicked':"country"}>
@@ -18,6 +19,7 @@ const handleClick = () =>{
       <p>Population: {population}</p>
       <p>Areas: {area}</p>
       <p><small>Code: {cca3}</small></p>
+      <button onClick={visitedCountry}>Mark as visited</button> <br /> <br />
       <button onClick={handleClick}>{visited ? "Visited":"Going"}</button>
       <p><small>{!visited?"I visited This country." : "Lets go"}</small></p>
     </div>
